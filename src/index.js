ecfe3822d89147ea65817431dcc7566ccab2cf7f
Igor-Novitski <pro100nke@gmail.com>
2022-02-21 22:20:38 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+   let objNum = {
+      0: 'zero',
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine',
+      10: 'ten',
+      11: 'eleven',
+      12: 'twelve',
+      13: 'thirteen',
+      14: 'fourteen',
+      15: 'fifteen',
+      16: 'sixteen',
+      17: 'seventeen',
+      18: 'eighteen',
+      19: 'nineteen',
+      20: 'twenty',
+      30: 'thirty',
+      40: 'forty',
+      50: 'fifty',
+      60: 'sixty',
+      70: 'seventy',
+      80: 'eighty',
+      90: 'ninety',
+   }
+
+   if (number <= 20) {
+      return objNum[number]
+   } else if (number > 20 && number < 100 && +number.toString()[1] == 0) {
+      return `${objNum[(number.toString()[0] + '0')]}`;
+   } else if (number > 99 && +number.toString()[1] == 0 && +number.toString()[2] == 0) {
+      return `${objNum[number.toString()[0]]} hundred`;
+   } else if (number > 20 && number < 100) {
+      return `${objNum[(number.toString()[0] + '0')]} ${objNum[number.toString()[1]]}`;
+   } else if (number > 100 && +number.toString()[1] < 2) {
+      return `${objNum[number.toString()[0]]} hundred ${objNum[+(number.toString()[1] + number.toString()[2]).toString()]}`;
+   } else if (number > 100 && +number.toString()[2] == 0) {
+      return `${objNum[number.toString()[0]]} hundred ${objNum[(number.toString()[1] + '0')]}`;
+   } else if (number > 100) {
+      return `${objNum[number.toString()[0]]} hundred ${objNum[(number.toString()[1] + '0')]} ${objNum[number.toString()[2]]}`;
+   }
 }
